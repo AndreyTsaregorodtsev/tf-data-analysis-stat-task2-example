b9452da9753434a3de2973c1653c07fc03fc860d
AndreyTsaregorodtsev <71283873+AndreyTsaregorodtsev@users.noreply.github.com>
2023-04-05 15:53:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,8 +13,8 @@ def solution(p: float, x: np.array) -> tuple:
     n = len(x)
     for i in range(n):
         s = s + (x[i])**2
-    Chi1 = chi2.ppf(1-alpha*0.5, n)
-    Chi2 = chi2.ppf(alpha*0.5, n)
+    Chi1 = chi2.ppf(1-alpha*0.5, 2*n)
+    Chi2 = chi2.ppf(alpha*0.5, 2*n)
     l = s/(42*Chi1)
     r = s/(42*Chi2)
     
